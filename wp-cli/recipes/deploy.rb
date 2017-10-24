e77a78f62fc4f0dc3032abb89284343b866257c0
EC2 Default User <ec2-user@ip-10-0-1-247.ap-northeast-1.compute.internal>
2017-10-24 03:00:40 +0000

    create wp-cli recipe

--- wp-cli/recipes/deploy.rb
+++ wp-cli/recipes/deploy.rb
@@ -33,7 +33,7 @@ execute "db create" do
 end
 
 execute "wp deploy" do
-   command "wp core install --url=#{public_hostname} --title=Test --admin_name=admin --admin_password=admin --admin_email=#{go.moto1.go@gmail.com}"
+   command "wp core install --url=#{public_hostname} --title=Test --admin_name=admin --admin_password=admin --admin_email=#{MakotoYaginuma@github.com}"
    cwd "#{wpdir}"
    user "deploy"
    action :run
